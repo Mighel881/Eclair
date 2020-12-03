33694e4d7410dad8e24dfbe60a9d7a7a19c32de8
Antique <official.antique@gmail.com>
2020-12-03 19:34:54 +0800

    Uploaded class files

---
+++ Eclair.h
@@ -0,0 +1,15 @@
+#import <UIKit/UIKit.h>
+
+@interface Eclair : UIView {
+  CAGradientLayer *leftGradient, *rightGradient;
+  CABasicAnimation *leftAnimation, *rightAnimation;
+}
+
+
+@property (nonatomic, assign) BOOL hidesWhenStopped;
+@property (nonatomic, assign) BOOL animating;
+
+
+-(void) startAnimating;
+-(void) stopAnimating;
+@end
